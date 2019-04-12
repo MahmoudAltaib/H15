c81cd2596095e4ffc9d6786b321de904105837ac
AElkady017 <49297467+AElkady017@users.noreply.github.com>
2019-04-12 14:54:36 +0200

    secondwork

--- Session2_work/Work2/main.c
+++ Session2_work/Work2/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 int main()
 {
-   float x = 5.001;
-   printf("%f \n",x);
-   if (x==4.999f+0.002f)
-   {
-       printf("Correct \n");
-   }
-   else
-    {
-       printf("Wrong \n");
-    }
+
+  uint32_t i8Fact = 1;
+  int32_t i8Num1 = 1;
+  int8_t i8Count = 1;
+
+  printf("Enter a number  :   ");
+  scanf("%d",&i8Num1);
+
+  for(i8Count = 1; i8Count <= i8Num1 ; i8Count++)
+  {
+      i8Fact = i8Fact * i8Count ;
+  }
+
+    printf("The factorial of %d is %u \n",i8Num1,i8Fact);
 }
