dc135b7bf76f799a43eb75d38131048f1947f953
AElkady017 <49297467+AElkady017@users.noreply.github.com>
2019-04-12 12:53:54 +0200

    intial commit

---
+++ Session2_work/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+
+int main()
+{
+
+int8_t i8Counter =0;
+
+     for(i8Counter = 1;i8Counter<=10;i8Counter++)
+     {
+         printf("%d \n",i8Counter);
+     }
+
+ return 0;
+
+}
