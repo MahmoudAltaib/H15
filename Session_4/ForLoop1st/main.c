c1754fa0117a66ca24a76d42a24e099b7be60dfe
MohamedShereef <49123956+MohamedShereef@users.noreply.github.com>
2019-04-12 12:08:41 +0200

    session_4_ForLoop_Ex

---
+++ Session_4/ForLoop1st/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"inttypes.h"
+int main()
+{
+    int8_t;
+    int i8Cnt=0;                       //allocate memory
+    for(i8Cnt=1;i8Cnt<=10;++i8Cnt)
+    {
+        printf("%d\n",i8Cnt);
+    }
+    return 0;
+}
