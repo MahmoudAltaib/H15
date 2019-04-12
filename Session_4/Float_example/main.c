1a17175fd5b422a90daeac70dfc3e8d60e627350
MohamedShereef <49123956+MohamedShereef@users.noreply.github.com>
2019-04-12 13:26:04 +0200

    Float_task

---
+++ Session_4/Float_example/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float x=5.001;
+
+   if(x==5.001f)
+   {
+       printf("correct\n");
+
+   }
+   else
+
+   printf("Wrong");
+
+    return 0;
+}
