6ba0c698c639c4288840f9c5b441a1ae22b6f4da
AhmedEssamAli <a.essam933@gmail.com>
2019-04-12 12:07:41 +0200

    For Loop

---
+++ For.Loop.Counter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "inttypes.h"
+
+int main()
+{
+    int8_t i8Cnt=0;
+for (i8Cnt=1;i8Cnt<=10;i8Cnt++)
+{
+    printf("%d\n",i8Cnt);
+}
+
+    return 0;
+}
