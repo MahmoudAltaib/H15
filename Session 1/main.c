2ac1bc8d3f58379cfa41dd92a3897538fdaa95e5
ahmed_sherif95 <ahmed_sherif95@hotmail.com>
2019-04-12 13:29:28 +0200

    Session 1 Submissions

--- Session 1/main.c
+++ Session 1/main.c
@@ -15,18 +15,32 @@ int main()
     z = x+y;
 
     printf("z = %03d",z);*/
-
+    //---------------------------------------------------------------------------------------------------
     /*if(printf("Ahmed"))
     {
 
     }*/
 
     //for(;;) // This is the infinite loop in C
-    int8_t i8Cnt;
+    //----------------------------------------------------------------------------------------------------
+    /*int8_t i8Cnt;
     for(i8Cnt = 1; i8Cnt<=10 ; i8Cnt++) // i8Cnt is used just for the naming convention to reach a clean code.
     {
        printf("%d\n", i8Cnt);
 
+    }*/
+    //----------------------------------------------------------------------------------------------------
+    float x = 5.001;
+    if(x == 5.001f)
+    {
+
+        printf("Correct");
     }
+    else
+    {
+        printf("Wrong");
+
+    }
+
     return 0;
 }
