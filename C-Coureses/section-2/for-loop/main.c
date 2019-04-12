41f46c7ea750d37416547177e340413d84f6ad9c
OKASHA-Mohamed <47341786+OKASHA-Mohamed@users.noreply.github.com>
2019-04-12 13:10:56 +0200

    for-loop

---
+++ C-Coureses/section-2/for-loop/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+int main()
+{
+     int8_t  i6Count = 0;
+        for(i6Count = 1; i6Count <= 10; i6Count++)
+        {
+            printf("%d\n",i6Count);
+        }
+    return 0;
+}
