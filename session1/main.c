86ea943838e381ea8030fc549803a37e0f1411d5
Ahmedatta1231 <49334438+Ahmedatta1231@users.noreply.github.com>
2019-04-12 09:27:45 +0200

    intial commit

---
+++ session1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int x=0;
+int y=0;
+int z=0;
+printf("enter x: \n");
+scanf("%d",&x);
+printf("enter y: \n");
+scanf("%d",&y);
+z=x+y;
+printf("z=: %d \n",z);
+    return 0;
+}
---
+++ session2/switch1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int x;
+printf("score is: ");
+switch(x)
+{
+case 1:(0)...(50);
+printf("fail");
+break;
+case 2 (51)...(60):
+printf("pass");
+break;
+case 3 (61)...(70):
+printf("good");
+break;
+case 4 (71)...(80):
+printf("verygood");
+break;
+case 5 (81)...(90):
+printf("excellent");
+break;
+default:
+    goto in;
+    break
+
+}
+
+    return 0;
+}
