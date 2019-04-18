e03bcb00cfe2f0da8d1c6610f81eb62d86c8be40
Ahmed Abdellatif <traqueduvampire@yahoo.fr>
2019-04-19 01:55:25 +0200

    IndiceSwap and related codes

--- BitIndiceSwap/main.c
+++ BitIndiceSwap/main.c
@@ -11,15 +11,13 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     uint8_t u8bit=0;
-    uint8_t u8shift=0;
     uint8_t u8radical=0; // radical to store mid 6 bits
     uint8_t u8bin=0;
-    uint8_t u8count=0;
     printf("Hello, Please Enter a Number in decimal : ");
     scanf("%d",&u8bit);
     u8radical = u8bit&0b01111110; //radical + clearing both front and tail of the byte
     u8bin = u8bit^u8radical;      // xoring to detect the differences between two sets
-    printf("%d \n",u8bin);
+    printf("%d \n",u8bin);        // just for readability
     if(u8bin==129)
     {
         printf("no need to swap cause both head and tail are 1s \n Res = %d",u8bit);
