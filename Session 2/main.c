1eb6de6196a559c6d91f730b50351ccd92e9f1bb
ahmed_sherif95 <ahmed_sherif95@hotmail.com>
2019-04-20 10:23:37 +0200

    Session 2 Submissions

---
+++ Session 2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "inttypes.h"
+int fact(int var);
+
+int main()
+{
+    unsigned int x = 0;
+    printf("please enter the number below\n");
+    scanf("%d", &x);
+    printf("Result is : %d",fact(x));
+
+
+    return 0;
+}
+
+int fact(int var)
+{
+    if(var == 1)
+    {
+        return 1;
+    }
+
+    return var * fact(var - 1);
+}
