9646c6fb480f01b8fb89e423d12383abc350751b
AElkady017 <49297467+AElkady017@users.noreply.github.com>
2019-04-27 05:25:29 +0200

    LinkedList

---
+++ Session8_Tasks/main.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 27, 2019
+ *      Author: A.Elkady
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+typedef struct stnode
+{
+	uint32_t u32Num ;
+	struct stnode *psNode ;
+}Node;
+
+int main()
+{
+	Node *peHead = 0;
+	Node *pe1    = 0;
+	Node *pe2    = 0;
+
+	peHead = (Node *) malloc(sizeof(Node));
+	pe1    = (Node *) malloc(sizeof(Node));
+	pe2    = (Node *) malloc(sizeof(Node));
+
+	peHead->u32Num = 75 ;
+	peHead->psNode   = pe1 ;
+
+	pe1->u32Num = 65 ;
+	pe1->psNode = pe2 ;
+
+	pe2->u32Num =  85 ;
+	pe2->psNode = 0 ;
+}
